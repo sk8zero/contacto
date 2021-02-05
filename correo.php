d2982f04720a82ef5803f82ada15a0f358f6894a
sk8zero <75801197+sk8zero@users.noreply.github.com>
2021-02-05 19:38:29 -0300

    Add files via upload

--- correo.php
+++ correo.php
@@ -1,5 +1,5 @@
 <?php
-    $destinatario = 'hola@alexcgdesing.com';
+    $destinatario = 'fragamarcos88@gmail.com';
     // esto es al correo al que se enviara el mensaje
 
     $nombre = $_POST['nombre'];
