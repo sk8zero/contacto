c792df52965519f7d884a498113ef7faf5502c7f
sk8zero <75801197+sk8zero@users.noreply.github.com>
2021-02-05 19:31:42 -0300

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario = 'hola@alexcgdesing.com'
+    // esto es al correo al que se enviara el mensaje
+
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email']; 
+
+    $header = "Enviado desde la pagina de AlexCG Disaign"
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script> alert('correo enviado exitosamente') </script>"
+    echo "<script>setTimeout(\"location.href='index.html'\",1000) </script>"
+?>
